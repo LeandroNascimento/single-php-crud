a2bccda9440073be0ad8c6e6ee7ca06e05d3749a
LeandroNascimento <fleandro.nascimento@gmail.com>
2015-09-14 14:31:41 -0300

    Adicionando o title

--- view/partials/_head.php
+++ view/partials/_head.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 	<head>
+		<title>Crud PHP</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		<style>
